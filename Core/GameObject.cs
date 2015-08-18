ce19edfb7c10f04fe2a98d159ed31a16a6817eeb
unknown <sahalha@LMB-113B-06.s2.rpn.ch>
2015-08-18 13:33:34 +0200

    Commit initial

---
+++ Core/GameObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LastArena.Core
+{
+    class GameObject
+    {
+        using Microsoft.Xna.Framework;
+    §   using Microsoft.Xna.Framework.Graphics;
+
+
+
+
+
+    }
+}
